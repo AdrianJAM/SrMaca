644f5f3dafed162b77bcdb6dcf6b8c9bb1515f8f
The Weed Doctor <adrianjam123@gmail.com>
2023-10-27 11:36:49 -0400

    first commit

---
+++ src/main/java/com/srmaca/SrmacaApplication.java
@@ -0,0 +1,13 @@
+package com.srmaca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrmacaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrmacaApplication.class, args);
+	}
+
+}
