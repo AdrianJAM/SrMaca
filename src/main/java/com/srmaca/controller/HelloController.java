f939cce57326576b4fd957488c55b26c2aa734c3
The Weed Doctor <adrianjam123@gmail.com>
2023-10-29 20:26:29 -0400

    hola mundo

---
+++ src/main/java/com/srmaca/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.srmaca.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hola")
+    public String holamundo(){
+        return  "Hola Mundo";
+    }
+}
