dab8da56fcf26322c0f238710dd580737e52c3bd
The Weed Doctor <adrianjam123@gmail.com>
2023-11-09 00:26:23 -0400

    endpoints ProductController

--- src/main/java/com/srmaca/repository/ProductRepository.java
+++ src/main/java/com/srmaca/repository/ProductRepository.java
@@ -1,19 +1,14 @@
 package com.srmaca.repository;
 
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.srmaca.model.ecommerce.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>{
-    //Metodo Para Buscar Producto mediante Nombre
-    Optional<Product> findByName(String name);
-    //Metodo Para Verificar si un producto existe en la bd
-    Boolean existsByName(String name);
-    //Metodo Para Buscar Producto mediante Id
-    Optional<Product> findByidProduct(Long idProduct);
-    //Metodo para obtener todos los productos
-    List<Product> findAll();
+    //Metodo para buscar por ID
+    List<Product> findByIdProduct(Long idProduct);
+    //Metodo para buscar por nombre
+    List<Product> findByName(String name);
 }
