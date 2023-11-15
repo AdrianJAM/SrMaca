8f13c230b98f02038352ef7946e84f3d0d0088fa
The Weed Doctor <adrianjam123@gmail.com>
2023-11-14 20:36:02 -0400

    changes names and clean cart

--- src/main/java/com/srmaca/controller/CartController.java
+++ src/main/java/com/srmaca/controller/CartController.java
@@ -1,31 +1,8 @@
 package com.srmaca.controller;
 
-import java.util.List;
-import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.srmaca.model.ecommerce.Cart;
-import com.srmaca.model.ecommerce.CartItem;
-import com.srmaca.model.ecommerce.Product;
-import com.srmaca.service.CartService;
-import com.srmaca.service.ProductService;
 
 @RestController
 @RequestMapping("/api/cart")
 public class CartController {
-    @Autowired
-    private CartService cartService;
-    //private ProductService productService;
-
-    public CartController(CartService cartService){
-        this.cartService = cartService;
-    }
-
-    @GetMapping("/{cartId}")
-    public ResponseEntity<Cart> getCartById(@PathVariable Long cartId) {
-        Optional<Cart> cart = cartService.getCartByIdCart(cartId);
-        return cart.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
-    }
 }
--- src/main/java/com/srmaca/service/cartService.java
+++ src/main/java/com/srmaca/service/cartService.java
@@ -1,22 +1,5 @@
 package com.srmaca.service;
 
-import java.math.BigDecimal;
-import java.util.List;
-import com.srmaca.model.ecommerce.Cart;
-import com.srmaca.model.ecommerce.CartItem;
-import com.srmaca.model.ecommerce.Product;
-
 public interface CartService {
-    //Metodo para agregar al carrito
-    void addToCart(Cart cart, Product product, int quantity);
-    //Metodo para remover del carrito
-    void removeFromCart(Cart cart, Product product);
-    //Metodo para borrar carrito
-    void clearCart(Cart cart);
-    //Metodo para calcular total del carrito - checkout
-    BigDecimal calculateTotal(Cart cart);
-    //Metodo para guardar/actualizar carrito
-    //void saveCart(Cart cart);
-    //Metodo par obtener items del carrito
-    List<CartItem> getCartItems(Cart cart);
+
 }
