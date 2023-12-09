6a6830e09a56b7c1471f39d2ababf7e1f9fc93f6
The Weed Doctor <adrianjam123@gmail.com>
2023-12-09 00:23:34 -0400

    continue stp jwt

--- src/main/java/com/srmaca/security/jwt/JwtUtil.java
+++ src/main/java/com/srmaca/security/jwt/JwtUtil.java
@@ -1,14 +1,22 @@
 package com.srmaca.security.jwt;
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties.Jwt;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import com.google.common.base.Function;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwsHeader;
+import io.jsonwebtoken.JwtParserBuilder;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtUtil {
@@ -19,6 +27,15 @@ public class JwtUtil {
         return extractClaims(token, Claims::getSubject);
     }
 
+    public Claims extractAllClaims(String token) {
+        SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+            return Jwts.parserBuilder()
+                        .setSigningKey(secretKey)
+                        .build()
+                        .parseClaimsJws(token)
+                        .getBody();
+    }
+
     public Date extractExpiration(String token) {
         return extractClaims(token, Claims::getExpiration);
     }
@@ -26,10 +43,6 @@ public class JwtUtil {
     public <T> T extractClaims(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
-    }
-
-    public Claims extractAllClaims(String token) {
-        return Jwts.parser().setSigningKey(secret).build().parseClaimsJws(token).getBody();
     }
 
     private Boolean isTokenExpired(String token) {
@@ -43,12 +56,14 @@ public class JwtUtil {
     }
 
     private String createToken(Map<String, Object> claims, String subject) {
+        Key key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
         return Jwts.builder()
-                    .setClaims(claims)
-                    .setSubject(subject)
-                    .setIssuedAt(new Date(System.currentTimeMillis()))
-                    .setExpiration(new Date(System.currentTimeMillis() + 100 * 60 * 60 * 10))
-                    .signWith(SignatureAlgorithm.HS256, secret).compact();
+                    .claims(claims)
+                    .claim("sub",subject)
+                    .claim("iat", new Date(System.currentTimeMillis()))
+                    .claim("exp", new Date(System.currentTimeMillis() + 100 * 60 * 60 * 10))
+                    .signWith(key)
+                    .compact();
     }
 
     public Boolean validateToken(String token, UserDetails userDetails) {
